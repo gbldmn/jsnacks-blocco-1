040d6e1d2b76340d8d47b95d7eda78eb7204f3ee
glbdmn <gabrieledamiani@hotmail.it>
2023-03-28 10:58:37 +0200

    fine primo esercizio

---
+++ asset/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+let numero = prompt("Inserisci un numero");
+let nemero2 = prompt("Inserisci un altro numero");
+
+console.log(numero, nemero2);
+
+if (numero > nemero2) {
+    console.log(numero );
+} else {
+    console.log(nemero2);
+}
