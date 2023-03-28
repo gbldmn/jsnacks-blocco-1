aaaf611564ee15eae2b9a3d2164fc0e1fa684434
glbdmn <gabrieledamiani@hotmail.it>
2023-03-28 11:54:50 +0200

    fine esercizi

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,57 @@
+// primo esercizio
+
+
+// let numero = parseInt (prompt("Inserisci un numero"));
+// let nemero2 = parseInt (prompt("Inserisci un altro numero"));
+
+// console.log(numero, nemero2);
+
+// if (numero > nemero2) {
+//     console.log(numero );
+// } else {
+//     console.log(nemero2);
 
 
 
-let numero = prompt("Inserisci un numero");
-let nemero2 = prompt("Inserisci un altro numero");
+// secondo esercizio
 
-console.log(numero, nemero2);
+// let parola1 =  prompt("Inserisci parola 1 ");
+// let parola2 =  prompt("Inserisci parola 2 ");
 
-if (numero > nemero2) {
-    console.log(numero );
-} else {
-    console.log(nemero2);
-}
+
+// if (parola1.length > parola2.length) {
+//     console.log(parola1);
+// } else if (parola1.length < parola2.length) {
+//     console.log(parola2);
+// } else if (parola.length == parola2.length) {
+//     console.log(`${parola1} e ${parola2} sono uguali`);
+// }
+
+
+// terzo esercizio
+
+//  let somma = 0;
+ 
+
+//  for (let i = 0; i < 10; i++) {
+//     let numero = parseInt (prompt("Inserisci un numero"));
+//     somma = somma + numero;
+// }
+
+//  console.log(`la somma totale è ${somma}`);
+
+
+
+
+// quarto esercizio
+
+// let arrayNumeri = [];
+
+// for (let i = 0; i < 6; i++) {
+//     let numero = parseInt (prompt("Inserisci un numero"));
+//     if (numero % 2 != 0) {
+//         arrayNumeri.push (numero);
+// }
+
+//  }
+//  console.log(arrayNumeri);
